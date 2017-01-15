980449f66c34e77ca59f4c8ca8dd7bb854b0f3c4
jbarcia <jbarcia99@yahoo.com>
2017-01-14 19:37:06 -0500

    Health Templates

---
+++ dirtycat.sh
@@ -0,0 +1,28 @@
+# /bin/bash
+
+sudo apt-get install zip unzip curl screen tmux apache2 php libapache2-mod-php php-mcrypt
+
+# Generate vhosts
+while read p; do
+site=$(echo $p | cut -f1 -d;)
+category=$(echo $p | cut -f2 -d;)
+
+mkdir -p /var/www/html/$site
+
+cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$site.conf
+ln -s /etc/apache2/sites-available/$site.conf /etc/apache2/sites-enabled/$site.conf
+
+sed -ie "s/\#ServerName www.example.com/ServerName $site\nServerAlias www.$site/g" /etc/apache2/sites-available/$site.conf
+sed -ie "s/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/html\/$site/g" /etc/apache2/sites-available/$site.conf
+
+a2ensite $site
+
+if $category == ""
+	$(( ( RANDOM % 43 )  + 1 ))
+	unzip ~/dental_clinic-web.zip -d /var/www/html/$site/
+	mv /var/www/html/$site/web/* /var/www/html/$site/
+
+done < hosts.txt
+
+
+service apache2 restart
