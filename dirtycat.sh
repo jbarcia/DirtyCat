41d05712cf14a809ce6a128b413962af97359957
jbarcia <jbarcia99@yahoo.com>
2017-01-14 20:08:56 -0500

    Health Templates

--- dirtycat.sh
+++ dirtycat.sh
@@ -9,21 +9,21 @@ category=$(echo $p | cut -f2 -d\;)
 
 echo $site
 echo $category
-#mkdir -p /var/www/html/$site
-#
-#cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$site.conf
-#ln -s /etc/apache2/sites-available/$site.conf /etc/apache2/sites-enabled/$site.conf
-#
-#sed -ie "s/\#ServerName www.example.com/ServerName $site\nServerAlias www.$site/g" /etc/apache2/sites-available/$site.conf
-#sed -ie "s/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/html\/$site/g" /etc/apache2/sites-available/$site.conf
-#
-#a2ensite $site
-#
-#if [ "$category" == 'HEALTH' ] || [ "$category" == 'health' ] || [ "$category" == 'Health' ]; then
-#	rand=$(( ( RANDOM % 43 )  + 1 ))
-#	unzip Templates/HEALTH/$rand*.zip -d /var/www/html/$site/
-#	mv /var/www/html/$site/web/* /var/www/html/$site/
-#fi
+mkdir -p /var/www/html/$site
+
+cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/$site.conf
+ln -s /etc/apache2/sites-available/$site.conf /etc/apache2/sites-enabled/$site.conf
+
+sed -ie "s/\#ServerName www.example.com/ServerName $site\nServerAlias www.$site/g" /etc/apache2/sites-available/$site.conf
+sed -ie "s/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/html\/$site/g" /etc/apache2/sites-available/$site.conf
+
+a2ensite $site
+
+if [ "$category" == 'HEALTH' ] || [ "$category" == 'health' ] || [ "$category" == 'Health' ]; then
+	rand=$(( ( RANDOM % 43 )  + 1 ))
+	unzip Templates/HEALTH/$rand*.zip -d /var/www/html/$site/
+	mv /var/www/html/$site/web/* /var/www/html/$site/
+fi
 
 done < $1
 
